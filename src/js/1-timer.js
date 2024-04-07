a5cea50febaa4b6466fe2d299e2c88ea2cddcdd3
Olena1iashenko <lyashenko.alenkaa@gmail.com>
2024-04-07 19:03:36 +0300

    renew

---
+++ src/js/1-timer.js
@@ -0,0 +1,21 @@
+// Описаний в документації
+import flatpickr from "flatpickr";
+// Додатковий імпорт стилів
+import "flatpickr/dist/flatpickr.min.css";
+
+const input = document.querySelector('#datetime-picker');
+let userSelectedDate;
+const startBtn = document.querySelector('[data-start]');
+startBtn.disabled = true;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+flatpickr(input, options);
